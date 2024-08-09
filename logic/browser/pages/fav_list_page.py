33a32c96ba5282161c6d3dc2c34b8751b0125850
jameel98 <jameelmograbi@gmail.com>
2024-08-09 14:38:00 +0300

    add test fav list by ui

--- logic/browser/pages/fav_list_page.py
+++ logic/browser/pages/fav_list_page.py
@@ -1,3 +1,6 @@
+import time
+
+from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,6 +10,8 @@ from infra.browser.base_page import BasePage
 
 class FavPage(BasePage):
     LIST_IS_EMPTY_LOCATOR = "//div[@class='message info empty']"
+    FAV_BUTTON_LOCATOR = '//li//div[@class="product_wishlist"]/div/a'
+
     def __init__(self, driver):
         super().__init__(driver)
 
@@ -38,5 +43,16 @@ class FavPage(BasePage):
 
     def get_list_is_empty_message(self):
         wait = WebDriverWait(self._driver, 10)
-        empy_list = wait.until(EC.element_to_be_clickable((By.XPATH, self.get_item_fav_button_locator(index))))
-        empy_list.text()
+        empy_list = wait.until(EC.presence_of_element_located((By.XPATH, self.LIST_IS_EMPTY_LOCATOR)))
+        return empy_list.text
+
+    def remove_all_elements(self):
+        wait = WebDriverWait(self._driver, 10)
+        try:
+            fav_button_list = wait.until(EC.presence_of_all_elements_located((By.XPATH, self.FAV_BUTTON_LOCATOR)))
+            if fav_button_list:
+                for _ in range(len(fav_button_list)):
+                    self.click_remove_from_fav(1)
+                    time.sleep(1)  # Add a short delay to allow for any page updates
+        except TimeoutException:
+            print("Favorite list is already empty. No elements to remove.")
--- test/test_fav_ui.py
+++ test/test_fav_ui.py
@@ -1,8 +1,8 @@
 import time
 import unittest
-from auth.auth_setup import Auth
-from infra.api.api_wrapper import APIWrapper
+
 from infra.browser.browser_wrapper import BrowserWrapper
+from logic.browser.components.login import Login
 from logic.browser.components.navbar import NavBar
 from logic.browser.pages.fav_list_page import FavPage
 from logic.browser.pages.search_result import SearchResult
@@ -15,25 +15,31 @@ class TestFavorite(unittest.TestCase):
         # Initialize driver
         self.browser_wrapper = BrowserWrapper()
         self.config = self.browser_wrapper.config
-        self.driver = self.browser_wrapper.get_driver(self.config["base_url"])
-        self.driver.get_cookie("form_key")
-        # start auth
-        self.auth = Auth(driver=self.driver)
-        self.auth.login_via_api_save_cookies()
-        self.auth.set_cookies()
+
         # open browser
-        self.driver.get(self.config["base_url"])
-        #search item by category
+        self.driver = self.browser_wrapper.get_driver(self.config["base_url"])
+        self.navbar = NavBar(self.driver)
+        # login via ui
+        self.nav_bar = NavBar(self.driver)
+        self.nav_bar.click_login()
+        self.login = Login(self.driver)
+        self.login.login_flow(self.config["email"], self.config["password"])
+
+        time.sleep(10)
+        # search item by category
         self.navbar = NavBar(self.driver)
         self.navbar.click_on_category(Category.LAST_CHANCE.value)
         self.search_result = SearchResult(self.driver)
 
     def tearDown(self):
+        self.fav_page.remove_all_elements()
+        self.fav_page.refresh_page()
+        time.sleep(5)
         self.driver.quit()
 
-    def test_add_item_to_favorite_page(self):
+    def test_add_item_to_fav_list(self):
         # act
-        self.search_result.get_item_fav_button_locator(1)
+        self.search_result.click_add_to_fav(1)
         element_id = self.search_result.get_element_id(1)
 
         # navigate to favlist page
@@ -43,13 +49,15 @@ class TestFavorite(unittest.TestCase):
 
         self.assertEqual(element_id2, element_id)
 
-    def test_remove_item_from_favorite_page(self):
-        self.search_result.get_item_fav_button_locator(1)
+    def test_remove_item_from_fav_list(self):
+        # arrange
+        self.search_result.click_add_to_fav(1)
         # navigate to favlist page
         self.navbar.navigate_to_fav_list()
         self.fav_page = FavPage(self.driver)
-
         # act
         self.fav_page.click_remove_from_fav(1)
+
         self.fav_page.refresh_page()
 
+        self.assertEqual(self.fav_page.get_list_is_empty_message(), "ברשימת המועדפים הזו אין פריטים")
